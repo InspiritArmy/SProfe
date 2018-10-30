9ca11648195231c10643e5a899f5c09ac6f033b3
Giaosw <giaosw@hotmail.com>
2018-10-29 20:34:59 -0500

    Add files via upload

---
+++ SprofeOficial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SprofeOficial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
